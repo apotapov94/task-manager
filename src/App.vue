482abdd7c66ebcdf174d2b195a62d3006f51e470
apotapov94 <62658363+apotapov94@users.noreply.github.com>
2023-12-06 17:10:48 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,53 @@
 <template>
   <div
     :class="`content ${opened ? 'active' : ''}`"
-    :style="opened ? `background-color: ${config.mainColor}` : ''"
+    :style="config.mainColor ? `background-color: ${config.mainColor}` : ''"
   >
     <div class="container">
       <SalesList :styles="config" />
     </div>
   </div>
+  <style>
+    .container {
+      max-width: 1200px;
+      margin: 0 auto;
+    }
+    .content {
+      height: 120px;
+      overflow: hidden;
+      transition: all 0.4s;
+      position: fixed;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      box-shadow: 8px 8px 23px #a1a1a1;
+    }
+    .content.active {
+      height: 500px;
+    }
+    .sales-list {
+      display: flex;
+      flex-direction: column;
+      gap: 8px;
+    }
+    .task-card {
+      display: flex;
+      justify-content: space-between;
+    }
+    .sales-widget__arrow {
+      cursor: pointer;
+      transition: all 0.4s;
+    }
+    .content.active .sales-widget__arrow {
+      transform: rotate(180deg);
+    }
+
+    .sales-widget__header {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+  </style>
 </template>
 
 <script>
@@ -29,45 +70,3 @@ export default {
   components: { SalesList }
 }
 </script>
-
-<style>
-.container {
-  max-width: 1200px;
-  margin: 0 auto;
-}
-.content {
-  height: 120px;
-  overflow: hidden;
-  transition: all 0.4s;
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  box-shadow: 8px 8px 23px #a1a1a1;
-}
-.content.active {
-  height: 500px;
-}
-.sales-list {
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
-}
-.task-card {
-  display: flex;
-  justify-content: space-between;
-}
-.sales-widget__arrow {
-  cursor: pointer;
-  transition: all 0.4s;
-}
-.content.active .sales-widget__arrow {
-  transform: rotate(180deg);
-}
-
-.sales-widget__header {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
-</style>
